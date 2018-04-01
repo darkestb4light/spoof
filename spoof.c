9660b8dda2305d4fcf856a95b4155ab320d299e9
darkestb4light <darkestb4light@gmail.com>
2018-04-01 16:02:09 -0700

    fortmat changes

--- spoof.c
+++ spoof.c
@@ -1,62 +1,59 @@
 /*
-	Purpose:
-		Demonstrate a PoC for dealing with raw sockets. The driver for this was to 
-		provide an interface for spoofing a source IPv4 address or hostname.
-
-		This is a proof of concept and is not intended to:
-		- Be free of bugs
-		- Be used maliciously
-		- Target any asset without prior authorization
-		- Be modified with any intent outside of personal research or learning
-		** See the license that should accompany this README and code.
-	Developer:
-		Ray Daley (https://github.com/darkestb4light)
-	Note:
-		To get deeper than the typical app layer, we need to deal with raw sockets. 
-		This requires elevated privileges since you could do evil things. 
-
-		The source argument passed will be inputted into the source address field 
-		of the IP header. 
-
-		If you feel like modifying TCP flags, see the definition constants below 
-		(prefixed with "TCP_FLAG_"). They can be enabled (1) or disabled (0). This 
-		will require a recompile upon each change. IMPORTANT: It is recommended to 
-		avoid modifying any values outside of TCP flags unless you know what you 
-		are doing.
-
-		On BSD systems (OSX and FREEBSD), spoofing 127.0.0.1 causes sendto(2) to 
-		fail with: "Can't assign requested address". Use another source address or 
+    Purpose:
+        Demonstrate a PoC for dealing with raw sockets. The driver for this 
+        was to provide an interface for spoofing a source IPv4 address or 
         hostname.
 
-		Compile: 
-		
-		$ gcc -D <TARGET-OS> -o spoof spoof.c
+        This is a proof of concept and is not intended to:
+        - Be free of bugs
+        - Be used maliciously
+        - Target any asset without prior authorization
+        - Be modified with any intent outside of personal research or learning
+        ** See the license that should accompany this README and code.
+    Developer:
+        Ray Daley (https://github.com/darkestb4light)
+    Note:
+        To get deeper than the typical app layer, we need to deal with raw 
+        sockets. This requires elevated privileges since you could do evil 
+        things. 
 
-		Where <TARGET-OS> is one of: LINUX, FREEBSD, or OSX (depending on the 
-		platform you are compiling on).
+        The source argument passed will be inputted into the source address 
+        field of the IP header. 
 
-		Usage: 
+        If you feel like modifying TCP flags, see the definition constants 
+        below (prefixed with "TCP_FLAG_"). They can be enabled (1) or disabled 
+        (0). This will require a recompile upon each change. IMPORTANT: It is 
+        recommended to avoid modifying any values outside of TCP flags unless 
+        you know what you are doing.
 
+        On BSD systems (OSX and FREEBSD), spoofing 127.0.0.1 causes sendto(2) 
+        to fail with: "Can't assign requested address". Use another source 
+        address or hostname.
+    Compile: 
+        $ gcc -D <TARGET-OS> -o spoof spoof.c
+
+        Where <TARGET-OS> is one of: LINUX, FREEBSD, or OSX (depending on the 
+        platform you are compiling on).
+    Usage: 
         spoof <src> <dst> <dport>
 
-		<src>	# source IPv4 address or hostname to spoof
-		<dst>	# destination IPv4 address or hostname of victim
-		<dport> # destination port to send spoofed TCP packet
-		
-		Execute (example 1):
+        <src>   # source IPv4 address or hostname to spoof
+        <dst>   # destination IPv4 address or hostname of victim
+        <dport> # destination port to send spoofed TCP packet
 
-		$ sudo ./spoof 123.45.67.8 192.168.0.10 55555
+        Execute (example 1):
+        
+        $ sudo ./spoof 123.45.67.8 192.168.0.10 55555
 
-		The above will spoof the source address "123.45.67.8" and send a packet 
-		to the destination "192.168.0.10" over TCP port 55555.
+        The above will spoof the source address "123.45.67.8" and send a 
+        packet to the destination "192.168.0.10" over TCP port 55555.
 
         Execute (example 2):
 
-		$ sudo ./spoof foobar.com my.victim.com 1337
+        $ sudo ./spoof foobar.com my.victim.com 1337
 
-		The above will spoof the source address "foobar.com" and send a packet 
-		to the destination "my.victim.com" over TCP port 1337.
-
+        The above will spoof the source address "foobar.com" and send a packet 
+        to the destination "my.victim.com" over TCP port 1337.
 */
 
 #include <errno.h>
@@ -116,21 +113,21 @@
 
 int main(int argc, char **argv)
 {
-	int                 res, dport, sd, pktsz, optval = 1;
-	char 	            *src, *dst;
-	unsigned char       data[0]; /* no data (payload) segment */
-    struct addrinfo     hints, *result;
-	struct sockaddr_in  sa_in, *saddr, *daddr;
-	struct tcphdr       tcp_hdr;
+    int			res, dport, sd, pktsz, optval = 1;
+    char		*src, *dst;
+    unsigned char	data[0]; /* no data (payload) segment */
+    struct addrinfo	hints, *result;
+    struct sockaddr_in	sa_in, *saddr, *daddr;
+    struct tcphdr	tcp_hdr;
 	
-	if(argc != MAX_ARG){
-		fprintf(stderr, "Usage: %s %s\n\n%s\n%s\n%s\n\n", 
-			NAME, "<src> <dst> <dport>", 
-			"<src>	 # source IPv4 address or hostname to spoof", 
-			"<dst>	 # destination IPv4 address or hostname of victim",
-			"<dport> # destination port to send spoofed TCP packet");              
-		return 1;
-	}
+    if(argc != MAX_ARG){
+        fprintf(stderr, "Usage: %s %s\n\n%s\n%s\n%s\n\n", 
+            NAME, "<src> <dst> <dport>", 
+            "<src>   # source IPv4 address or hostname to spoof", 
+            "<dst>   # destination IPv4 address or hostname of victim",
+            "<dport> # destination port to send spoofed TCP packet");              
+        return 1;
+    }
     
     /* setup source IPv4 / hostname */
     if((src = (char *) malloc(HOST_SZ)) == NULL){
@@ -173,20 +170,20 @@ int main(int argc, char **argv)
     /* set destination port for sending packet */
     dport = atoi(argv[3]);
     
-	if(dport < 0 || dport > 65535){
-		fprintf(stderr, "%s: destination port outside of range: %d\n", NAME, dport);
-		return 1;
-	}
+    if(dport < 0 || dport > 65535){
+        fprintf(stderr, "%s: destination port outside of range: %d\n", NAME, dport);
+        return 1;
+    }
 
     /* socket address */
-	sa_in.sin_family = AF_INET;
-	sa_in.sin_port = htons(dport);
-	sa_in.sin_addr.s_addr = inet_addr(dst);
+    sa_in.sin_family = AF_INET;
+    sa_in.sin_port = htons(dport);
+    sa_in.sin_addr.s_addr = inet_addr(dst);
 
 #ifdef LINUX
-	struct iphdr ip_hdr;
+    struct iphdr ip_hdr;
 	
-	/* IP header */	
+    /* IP header */	
     ip_hdr.ihl = IP_HDRLEN;
     ip_hdr.version = IP_VER;
     ip_hdr.tos = IP_TYPEOFSERV;
@@ -195,34 +192,34 @@ int main(int argc, char **argv)
     ip_hdr.ttl = IP_TIMETOLIVE;
     ip_hdr.protocol = IP_PROTO;
     ip_hdr.check = IP_CHKSUM;
-	ip_hdr.id = htons(rand() % 65535);
+    ip_hdr.id = htons(rand() % 65535);
     ip_hdr.saddr = inet_addr(src);
     ip_hdr.daddr = inet_addr(dst);  
 	
-	/* TCP header */
-	tcp_hdr.doff = TCP_DATAOFF;
-	tcp_hdr.fin = TCP_FLAG_FIN;
-	tcp_hdr.syn = TCP_FLAG_SYN;
-	tcp_hdr.rst = TCP_FLAG_RST;
-	tcp_hdr.psh = TCP_FLAG_PSH;
-	tcp_hdr.ack = TCP_FLAG_ACK;
+    /* TCP header */
+    tcp_hdr.doff = TCP_DATAOFF;
+    tcp_hdr.fin = TCP_FLAG_FIN;
+    tcp_hdr.syn = TCP_FLAG_SYN;
+    tcp_hdr.rst = TCP_FLAG_RST;
+    tcp_hdr.psh = TCP_FLAG_PSH;
+    tcp_hdr.ack = TCP_FLAG_ACK;
     tcp_hdr.urg = TCP_FLAG_URG;
-	tcp_hdr.ece = TCP_FLAG_ECE;	
-	tcp_hdr.cwr = TCP_FLAG_CWR;
-	tcp_hdr.check = TCP_CHECKSUM;
-	tcp_hdr.window = htons(0);
-	tcp_hdr.urg_ptr = htons(0);
-	tcp_hdr.source = htons(rand() % 65535);
-	tcp_hdr.dest = htons(dport);
-	tcp_hdr.seq = htonl(0);
-	tcp_hdr.ack_seq = htonl(0);
-	
-	ip_hdr.tot_len = htons(sizeof(ip_hdr) + sizeof(tcp_hdr) + sizeof(data));
+    tcp_hdr.ece = TCP_FLAG_ECE;
+    tcp_hdr.cwr = TCP_FLAG_CWR;
+    tcp_hdr.check = TCP_CHECKSUM;
+    tcp_hdr.window = htons(0);
+    tcp_hdr.urg_ptr = htons(0);
+    tcp_hdr.source = htons(rand() % 65535);
+    tcp_hdr.dest = htons(dport);
+    tcp_hdr.seq = htonl(0);
+    tcp_hdr.ack_seq = htonl(0);
+
+    ip_hdr.tot_len = htons(sizeof(ip_hdr) + sizeof(tcp_hdr) + sizeof(data));
 #elif FREEBSD | OSX
-	struct ip ip_hdr;
-	unsigned int tcp_flags = 0;
+    struct ip ip_hdr;
+    unsigned int tcp_flags = 0;
 	
-	/* IP header */	
+    /* IP header */	
     ip_hdr.ip_hl = IP_HDRLEN;
     ip_hdr.ip_v = IP_VER;
     ip_hdr.ip_tos = IP_TYPEOFSERV;
@@ -231,73 +228,74 @@ int main(int argc, char **argv)
     ip_hdr.ip_ttl = IP_TIMETOLIVE;
     ip_hdr.ip_p = IP_PROTO;
     ip_hdr.ip_sum = IP_CHKSUM;
-	ip_hdr.ip_id = htons(rand() % 65535);
-	ip_hdr.ip_src.s_addr = inet_addr(src);
+    ip_hdr.ip_id = htons(rand() % 65535);
+    ip_hdr.ip_src.s_addr = inet_addr(src);
     ip_hdr.ip_dst.s_addr = inet_addr(dst);
-	
-	/* TCP header */
-	tcp_hdr.th_off = TCP_DATAOFF;
-	if(TCP_FLAG_FIN) tcp_flags |= TH_FIN;
-	if(TCP_FLAG_SYN) tcp_flags |= TH_SYN;
-	if(TCP_FLAG_RST) tcp_flags |= TH_RST;
-	if(TCP_FLAG_PSH) tcp_flags |= TH_PUSH;
-	if(TCP_FLAG_ACK) tcp_flags |= TH_ACK;
-	if(TCP_FLAG_URG) tcp_flags |= TH_URG;
-	if(TCP_FLAG_ECE) tcp_flags |= TH_ECE;
-	if(TCP_FLAG_CWR) tcp_flags |= TH_CWR;
-	tcp_hdr.th_flags = tcp_flags;
-	tcp_hdr.th_sum = TCP_CHECKSUM;
-	tcp_hdr.th_win = htons(0);
-	tcp_hdr.th_urp = htons(0);
-	tcp_hdr.th_sport = htons(rand() % 65535);
-	tcp_hdr.th_dport = htons(dport);
-	tcp_hdr.th_seq = htonl(0);
-	tcp_hdr.th_ack = htonl(0);
-	
-	if(AVOID_IPLEN_HTONS)
-		ip_hdr.ip_len = sizeof(ip_hdr) + sizeof(tcp_hdr) + sizeof(data);
-	else
-		ip_hdr.ip_len = htons(sizeof(ip_hdr) + sizeof(tcp_hdr) + sizeof(data));
-#else	/* should never fire if macro definitions are in sync */
-	fprintf(stderr, "%s: Target OS macro integrity issue. Aborting.\n", NAME);
-	return 1;
+
+    /* TCP header */
+    tcp_hdr.th_off = TCP_DATAOFF;
+    if(TCP_FLAG_FIN) tcp_flags |= TH_FIN;
+    if(TCP_FLAG_SYN) tcp_flags |= TH_SYN;
+    if(TCP_FLAG_RST) tcp_flags |= TH_RST;
+    if(TCP_FLAG_PSH) tcp_flags |= TH_PUSH;
+    if(TCP_FLAG_ACK) tcp_flags |= TH_ACK;
+    if(TCP_FLAG_URG) tcp_flags |= TH_URG;
+    if(TCP_FLAG_ECE) tcp_flags |= TH_ECE;
+    if(TCP_FLAG_CWR) tcp_flags |= TH_CWR;
+    tcp_hdr.th_flags = tcp_flags;
+    tcp_hdr.th_sum = TCP_CHECKSUM;
+    tcp_hdr.th_win = htons(0);
+    tcp_hdr.th_urp = htons(0);
+    tcp_hdr.th_sport = htons(rand() % 65535);
+    tcp_hdr.th_dport = htons(dport);
+    tcp_hdr.th_seq = htonl(0);
+    tcp_hdr.th_ack = htonl(0);
+
+    if(AVOID_IPLEN_HTONS)
+        ip_hdr.ip_len = sizeof(ip_hdr) + sizeof(tcp_hdr) + sizeof(data);
+    else
+        ip_hdr.ip_len = htons(sizeof(ip_hdr) + sizeof(tcp_hdr) + sizeof(data));
+#else /* should never fire if macro definitions are in sync */
+    fprintf(stderr, "%s: Target OS macro integrity issue. Aborting.\n", NAME);
+    return 1;
 #endif
     
-	/* create socket */
-	if((sd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) == -1){
-		fprintf(stderr, "%s: socket: %s\n", NAME, strerror(errno));
-		return 1;
-	}
+    /* create socket */
+    if((sd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) == -1){
+        fprintf(stderr, "%s: socket: %s\n", NAME, strerror(errno));
+        return 1;
+    }
     	
-	printf("%s: socket descriptor created.\n", NAME);
+    printf("%s: socket descriptor created.\n", NAME);
 	
-	/* set socket options */
-	if(setsockopt(sd, IPPROTO_IP, IP_HDRINCL, &optval, sizeof(optval)) < 0){
-		fprintf(stderr, "%s: setsockopt: %s\n", NAME, strerror(errno));
-		return 1;
-	}
+    /* set socket options */
+    if(setsockopt(sd, IPPROTO_IP, IP_HDRINCL, &optval, sizeof(optval)) < 0){
+        fprintf(stderr, "%s: setsockopt: %s\n", NAME, strerror(errno));
+        return 1;
+    }
 	
-	printf("%s: socket options set.\n", NAME);
+    printf("%s: socket options set.\n", NAME);
 	
-	/* build packet */
+    /* build packet */
     pktsz = sizeof(ip_hdr) + sizeof(tcp_hdr) + sizeof(data);
-	unsigned char pkt[pktsz];
-	memcpy(pkt, &ip_hdr, sizeof(ip_hdr));
-	memcpy(pkt + sizeof(ip_hdr), &tcp_hdr, sizeof(tcp_hdr));
-	memcpy(pkt + sizeof(ip_hdr) + sizeof(tcp_hdr), data, sizeof(data));
+    unsigned char pkt[pktsz];
+    memcpy(pkt, &ip_hdr, sizeof(ip_hdr));
+    memcpy(pkt + sizeof(ip_hdr), &tcp_hdr, sizeof(tcp_hdr));
+    memcpy(pkt + sizeof(ip_hdr) + sizeof(tcp_hdr), data, sizeof(data));
 	
-	/* send packet */
-	if(sendto(sd, pkt, sizeof(pkt), 0, (struct sockaddr *)&sa_in, sizeof(sa_in)) < 0){
-		fprintf(stderr, "%s: sendto: %s\n", NAME, strerror(errno));
-		return 1;
-	}
+    /* send packet */
+   if(sendto(sd, pkt, sizeof(pkt), 0, (struct sockaddr *)&sa_in, sizeof(sa_in)) < 0){
+       fprintf(stderr, "%s: sendto: %s\n", NAME, strerror(errno));
+       return 1;
+    }
 
-	printf("%s: packet sent from %s to %s:%d.\n", NAME, src, dst, dport);
+    printf("%s: packet sent from %s to %s:%d.\n", NAME, src, dst, dport);
 
     /* clean up */
     free(src);
     free(dst);
     freeaddrinfo(result);
-    
-	return 0;
+
+    return 0;
 }
+
